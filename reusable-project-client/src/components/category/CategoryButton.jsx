19963c98174d9f0f415b8da618641bfdd2d98fee
nasirhpatwary <nasirhpatwary@gmail.com>
2025-06-29 13:13:30 +0600

    recaptch completed

--- reusable-project-client/src/components/category/CategoryButton.jsx
+++ reusable-project-client/src/components/category/CategoryButton.jsx
@@ -7,7 +7,7 @@ const CategoryButton = ({ label, name, isActive, onClick, image }) => {
         onClick={() => onClick(label)}
       >
         <img className="w-16 h-16 mx-auto rounded-full absolute transition-all duration-700 group-hover:opacity-80  right-0 left-0" src={image} alt={name} />
-        <CiCirclePlus className="w-16 h-16 mx-auto opacity-0 group-hover:opacity-100 transition-all duration-700 rounded-full absolute right-0 group-hover:bottom-7 bottom-0 left-0"  />
+        <CiCirclePlus className="w-16 group-hover:bg-black/30 text-white h-16 mx-auto opacity-0 group-hover:opacity-100 transition-all duration-700 rounded-full absolute right-0 group-hover:bottom-7.5 bottom-0 left-0"  />
         <h2 className={`text-center absolute left-0 right-0 bottom-2 ${isActive && "text-indigo-500"}`}>
           {name}
         </h2>
